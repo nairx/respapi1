2733f72b65bb396824d697450728ff535820f2f0
nairx <nairxprv@gmail.com>
2026-08-18 15:43:31 +0530

    first commit

---
+++ src/test/java/com/example/respapi1/Respapi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.respapi1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Respapi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
